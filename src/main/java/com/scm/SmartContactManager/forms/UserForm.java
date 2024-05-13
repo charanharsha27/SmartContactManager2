26a2c364748c5edf4700b3bbb611379c254eb4b2
charanharsha27 <charanharsha097@gmail.com>
2024-05-14 01:32:13 +0530

    made changes to bean validation and added password validator

--- src/main/java/com/scm/SmartContactManager/forms/UserForm.java
+++ src/main/java/com/scm/SmartContactManager/forms/UserForm.java
@@ -3,7 +3,7 @@ package com.scm.SmartContactManager.forms;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -24,6 +24,7 @@ public class UserForm {
     @NotBlank(message = "Email field is required")
     private String email;
     @NotBlank(message = "Password field is required")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$", message = "Meet all the below specified password criteria")
     private String password;
     @NotBlank(message = "About field is required")
     private String about;
